6f5155d0dfb0db8345bcc96a4d1d70ef5cddbb61
VectoRover <47414512+VectoRover@users.noreply.github.com>
2020-05-27 20:15:26 +0300

    Update Task2_C.c

--- Task2_C.c
+++ Task2_C.c
@@ -1,102 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <limits.h>
 
-int Search(FILE* file, char* word);
-int Search(FILE* file, char* word){
-    char current;
-    char* temp;
-    int length = 0, number_of_quots = 0, letter_flag = 0, len_of_temp = 0, min_word;
-    while (fscanf(file, "%c", &current) == 1){
-        length++;
-    }
-    min_word = INT_MAX;
-    temp = (char*)malloc(len_of_temp);
-    fseek(file, 0, 0);
-    if (length < 3){
-        printf("Error: incorrect input\n");
-        exit(-1);
-    }
-    for (int i = 0; i < length; i++){
-        fscanf(file, "%c", &current);
-        if (current == '"'){
-            number_of_quots++;
-        }
-        if ((current == '"') && (number_of_quots%2 == 0)){
-            letter_flag = 0;
-            for (int index = 0; index < len_of_temp; index++){
-                if ((temp[index] >= 65) && (temp[index] <= 90)){
-                    letter_flag++;
-                }
-                if (letter_flag == len_of_temp){
-                    if (temp[0] == 'A'){
-                        if (len_of_temp < min_word){
-                            word = (char*)realloc(word, (len_of_temp+1)*sizeof(char));
-                            for (int letter_index = 0; letter_index < len_of_temp + 1; letter_index++){
-                                word[letter_index] = temp[letter_index];
-                            }
-                        }
-                    }
-                }
+int func(const char* filename, char* slovo);
+void autotest(void);
+
+int func(const char* filename, char* slovo){
+FILE *input;
+int b=0;
+char str[512],word[512], *sbegin, *send;
+int min=INT_MAX;
+input=fopen(filename, "r");
+if (input)
+    {
+    while(fgets(str,256,input))
+    {
+        for(sbegin=strchr(str, '"'), send=(sbegin?strchr(sbegin+1,'"'):NULL);
+        sbegin&&send;
+        sbegin=strchr(send+1,'"'),send=(sbegin?strchr(sbegin+1,'"'):NULL))
+        {
+            strncpy(word,sbegin+1,send-sbegin-1);
+             word[send-sbegin-1]='\0';
+             if(strlen(word) <= min && *word <= 'A' && *word <= 'Z' && word[0] == 'A')
+            {
+              b = 1;
+              min=strlen(word);
+              slovo[0]='\0';
+              strcpy(slovo,word);
             }
-            len_of_temp = 0;
-            temp = (char*)realloc(temp, 0* sizeof(char));
-            continue;
-        }
-        if ((current != '"') && (number_of_quots%2 == 1)){
-            len_of_temp++;
-            temp = (char*)realloc(temp, sizeof(char)*(len_of_temp+1));
-            temp[len_of_temp -1] = current;
-            temp[len_of_temp] = '\0';
         }
     }
-    free(temp);
-    return 0;
+}
+else
+    {
+        printf("Невозможно открыть файл");
+        return -5;
+    }
+fclose(input);
+return b;
 }
 
-int Autotest(void);
-int Autotest(void){
-    FILE* test_file;
-    char* test_word;
-    test_file = fopen("F:\\prog\\test.txt", "r");
-    test_word = (char*)malloc(0 * sizeof(char));
-    if (!test_file){
-        printf("Error: cannot open test file\n");
-        exit(-1);
-    }
-    Search(test_file, test_word);
-    if (test_word[0] == 'A'){
-        printf("Autotest passed\n");
-    }
-    else{
-        printf("Autotest not passed\n");
-        exit(-1);
-    }
-    free(test_word);
-    fclose(test_file);
-    return 0;
+void autotest(void){
+char slovo[256];
+func("text.txt", slovo);
+if (strlen(slovo)==2){
+    printf("Автотест сдан\n");
+}
+else {printf("Автотест провален\n");}
 }
 
 int main(void){
-    FILE* input_file;
-    char* answer;
-    input_file = fopen("F:\\prog\\test.txt", "r");
-    answer = (char*)malloc(0 * sizeof(char));
-    answer[0] = 'x';
-    if (!input_file){
-        printf("Error: cannot open file\n");
-        exit(-1);
-    }
-    Autotest();
-    Search(input_file, answer);
-    if (answer[0] == 'x'){
-        printf("No such words!\n");
-    }
-    else{
-        printf("Answer is %s\n", answer);
-    }
-    free(answer);
-    fclose(input_file);
-    return 0;
+  char slovo[512];
+  int b;
+  b = func("test.txt", slovo);
+  if (b == 1){
+    printf("%s\n", slovo);
+           }
+  else{
+    printf("Такого слова нет\n");
+      }
+  autotest();
 }
